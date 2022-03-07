fa25aaebf76fcb8f928a596c93c178d2f54e9063
Ddovee <100857813+Ddovee@users.noreply.github.com>
2022-03-08 07:36:11 +0800

    Add files via upload

---
+++ 2.9.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Enter two numbers" << endl;
+	int a,b,c;
+	cin >> b >> c;//输入b和c 
+	a = b * c;//将bc的乘积赋值给a
+	cin >> a >> b >> c;//输入三个数并赋值给a,b,c 
+	return 0;
+}
